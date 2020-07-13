fad7a02e0fbdd8e9dd490e2a45066f64fcfaa9db
seungin yang <rhfpdk92@naver.com>
2020-07-13 22:05:21 +0900

    loading fonts

--- asset-management/src/index.js
+++ asset-management/src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './Icon.jpg';
 
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
+
+   const myIcon = new Image();
+   myIcon.src = Icon;
+   myIcon.height = 200;
+   myIcon.width = 200;
+
+
+   element.appendChild(myIcon);
+
   return element;
 }
 
--- asset-management/webpack.config.js
+++ asset-management/webpack.config.js
@@ -13,8 +13,20 @@ module.exports = {
              use: [
                'style-loader',
                'css-loader',
-             ],
+             ]
            },
-         ],
-       },
+           {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              'file-loader'
+              ]
+           },
+           {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+              'file-loader'
+              ]
+           }
+         ]
+       }
 };
