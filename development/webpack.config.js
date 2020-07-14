c82b3b3a3f10d3806241cd87ed2cd7afbbf715b8
seungin yang <rhfpdk92@naver.com>
2020-07-14 18:23:08 +0900

    add development tools

---
+++ development/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+
+  },
+  devtool: 'inline-source-map',
+  devServer:{
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  
+};
